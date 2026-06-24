2734539dad92ee7c9514145031061ce39de9927b
MusaMacheke <musamacheke.01@gmail.com>
2026-06-24 15:29:12 +0200

    sixth commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://fastlane-retail-api.onrender.com';
+const API_URL = 'https://fastlane-retail-api.onrender.com/api';
 
 export const getProducts = async () => {
   const response = await axios.get(`${API_URL}/products`);
